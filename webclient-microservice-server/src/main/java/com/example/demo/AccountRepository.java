5eadbb9395352ee33f5259f20a8689bf9be5d8cd
91805 <91805@DESKTOP-IP7HAUI>
2020-09-07 12:50:43 +0530

    First Commit

---
+++ webclient-microservice-server/src/main/java/com/example/demo/AccountRepository.java
@@ -0,0 +1,17 @@
+/**
+ * 
+ */
+package com.example.demo;
+
+import java.util.List;
+
+/**
+ * @author Dinesh.Rajput
+ *
+ */
+public interface AccountRepository {
+	
+	List<Account> getAllAccounts();
+	
+	Account getAccount(String number);
+}
